74443ece7cb9dccb0ce003fe312463593d455a33
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-07-29 16:02:22 +0900

    Create 3_fusioncatcher.sh

---
+++ sh/rna/3_fusioncatcher.sh
@@ -0,0 +1,7 @@
+##fusioncatcher.sh 
+cat config | while read id 
+do
+echo "start fusioncatcher for ${id}" `date`
+fusioncatcher.py -d /home/eunji/miniconda3/envs/python2/share/fusioncatcher-1.20/db/human_v98/ -i ./1.raw_fq/${id}/ -o ./7.fusion/${id}/ >> ./7.fusion/${id}_fusioncatcher.log 2>&1
+echo "end fusioncatcher for ${id}" `date`
+done 
