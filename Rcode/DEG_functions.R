b64f88a89c2e4db567dac4e6907fc30bc22acca6
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-08-11 12:13:16 +0900

    Update DEG_functions.R

--- Rcode/DEG_functions.R
+++ Rcode/DEG_functions.R
@@ -89,6 +89,22 @@ edgeR.2 <- function(count,group,ID){
   return(tccRES)
 }
 
+#TPM FPKM normalization 
+countToTpm <- function(counts, effLen){
+  rate <- log(counts) - log(effLen)
+  denom <- log(sum(exp(rate)))
+  exp(rate - denom + log(1e6))}
+
+countToFpkm <- function(counts, effLen){
+  N <- sum(counts)
+  exp( log(counts) + log(1e9) - log(effLen) - log(N) )}
+
+fpkmToTpm <- function(fpkm){
+  exp(log(fpkm) - log(sum(fpkm)) + log(1e6))}
+
+countToEffCounts <- function(counts, len, effLen){
+  counts * (len / effLen)}                  
+                  
 #df=resdata(dataframe) , gene=gene(character), raw=count
 heatmap <- function(df,gene,raw){
   suppressMessages(library(magrittr))
