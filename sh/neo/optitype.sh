474a584679fa3525ed64946a24d635f2159116cd
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-09-25 14:13:22 +0900

    Update optitype.sh

--- sh/neo/optitype.sh
+++ sh/neo/optitype.sh
@@ -1,3 +1,18 @@
+#RT-neoantigen prediction                                                       
+#SRR12060755-HCC1143 , SRR12060763-HCC38, SRR12060810-HCC1187 SRR12060756-HCC1395                                                                              
+#mkdir                                                                          
+#mkdir -p 1.raw_fq 2.clean_fq 3.qc/{raw_qc,clean_qc} 4.align/{hisat2_tran}      
+
+
+#cd ./0.sra                                                                     
+## fasterq-dump                                                                 
+#cat config | while read id                                                     
+#do                                                                             
+        #time fasterq-dump -3 -e 16 ${id}.sra -O ../1.raw_fq --outfile ${id}.fastq                                                                             
+#done                                                                           
+
+
+
 # input bam file - HLA typing                                                   
 
 ref=/home/eunji/proj/0_sh/ref/rna/hla_reference_rna.fasta
@@ -23,15 +38,12 @@ done
 # separate paired fastq file (/1 /2)                                            
 cat config | while read id
 do
-awk 'BEGIN{OFS="\n"} $0~/\/1$/{header = $0; getline seq; getline qheader; getli\
-ne qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_1.fastq
-awk 'BEGIN{OFS="\n"} $0~/\/2$/{header = $0; getline seq; getline qheader; getli\
-ne qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_2.fastq
+awk 'BEGIN{OFS="\n"} $0~/\/1$/{header = $0; getline seq; getline qheader; getline qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_1.fastq
+awk 'BEGIN{OFS="\n"} $0~/\/2$/{header = $0; getline seq; getline qheader; getline qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_2.fastq
 done
 
 #HLA typing                                                                     
 cat config | while read id
 do
-OptiTypePipeline.py -i ${id}_1.fastq ${id}_2.fastq --rna -v -o ./ -p ${id}.opti\
-type.rna
+OptiTypePipeline.py -i ${id}_1.fastq ${id}_2.fastq --rna -v -o ./ -p ${id}.optitype.rna
 done
