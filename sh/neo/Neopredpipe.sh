00ac07d5315b56504e72c20580d8df7ec9646c12
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-05 17:26:48 +0900

    Update Neopredpipe.sh

--- sh/neo/Neopredpipe.sh
+++ sh/neo/Neopredpipe.sh
@@ -39,3 +39,38 @@ echo "start samtools view for ${id}" `date`
 samtools view -Sb ./4.align/hisat2_tran/${id}.sam > ./4.align/hisat2_tran/${id}.bam
 echo "end samtools view for ${id}" `date`
 done
+
+# input bam file - HLA typing                                                   
+
+ref=/home/eunji/proj/0_sh/ref/rna/hla_reference_rna.fasta
+
+#bam2fq                                                                         
+cat config | while read id
+do
+samtools bam2fq ./4.align/hisat2_tran/${id}.bam > ${id}.fastq
+done
+
+#mapping to hla reference                                                       
+cat config | while read id
+do
+razers3 -i 95 -m 1 -dr 0 --thread-count 10 -o ${id}.bam ${ref} ${id}.fastq
+done
+
+#bam2fq                                                                         
+cat config | while read id
+do
+samtools bam2fq ${id}.bam > ${id}.fastq
+done
+
+# separate paired fastq file (/1 /2)                                            
+cat config | while read id
+do
+awk 'BEGIN{OFS="\n"} $0~/\/1$/{header = $0; getline seq; getline qheader; getline qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_1.fastq
+awk 'BEGIN{OFS="\n"} $0~/\/2$/{header = $0; getline seq; getline qheader; getline qseq; print header, seq, qheader, qseq}' ${id}.fastq > ${id}_2.fastq
+done
+
+#HLA typing                                                                     
+cat config | while read id
+do
+OptiTypePipeline.py -i ${id}_1.fastq ${id}_2.fastq --rna -v -o ./ -p ${id}.optitype.rna
+done
