e3ae95cd6d18f28d0a0b7f303298104ba888209e
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-19 18:58:00 +0900

    Update Neopredpipe.sh

--- sh/neo/Neopredpipe.sh
+++ sh/neo/Neopredpipe.sh
@@ -1,3 +1,17 @@
+
+#python=2.7 annovar netMHCpan-4.1 netMHCIIpan-3.2 PeptideMatch
+conda activate neopredpipe 
+git clone https://github.com/MathOnco/NeoPredPipe.git
+#Configure the 'usr_path.ini' file
+
+#https://www.uniprot.org/news/2005/02/01/release > uniprot_sprot.fasta
+#https://research.bioinformatics.udel.edu/peptidematch/commandlinetool.jsp , peptidematch
+wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+java -jar /home/eunji/proj/0_sh/biosoft/PeptideMatchCMD_src_1.0/PeptideMatchCMD_1.0.jar -a index -d uniprot_sprot.fasta -i sprot_index
+
+
+
+
 #https://github.com/MathOnco/NeoPredPipe
 python NeoPredPipe.py \
 -I somatic.vcf \
