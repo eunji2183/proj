e3eb41f2db24624c4313fc625217c256d393d54a
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-20 15:33:39 +0900

    Create pvacseq.sh

---
+++ sh/neo/pvacseq.sh
@@ -0,0 +1,8 @@
+conda create -n pvac #(python3)
+conda create -n pvac2 #(python2) 
+##annotation tool - VEP 
+#https://m.blog.naver.com/PostView.nhn?blogId=glshh13&logNo=221565564066&proxyReferer=https:%2F%2Fwww.google.com%2F -> VEP install
+conda install python=3
+pip install pvactools
+pip install pvactools --upgrade
+#https://pvactools.readthedocs.io/en/latest/install.html
