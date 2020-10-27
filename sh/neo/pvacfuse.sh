2b6829c542ead8a46318b04de2bb17365c538daa
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-27 17:32:24 +0900

    Update pvacfuse.sh

--- sh/neo/pvacfuse.sh
+++ sh/neo/pvacfuse.sh
@@ -5,6 +5,10 @@ conda install bwa python gcc_linux-64 ucsc-gtftogenepred samtools bedtools matpl
 wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.gtf.gz
 gtfToGenePred -genePredExt -geneNameAsName2 Homo_sapiens.GRCh38.84.gtf Homo_sapiens.GRCh38.84.genePred
+cut -f 1-10,12 Homo_sapiens.GRCh38.84.genePred > tmp.txt
+echo -e "#GRCh38.ensGene.name\tGRCh38.ensGene.chrom\tGRCh38.ensGene.strand\tGRCh38.ensGene.txStart\tGRCh38.ensGene.txEnd\tGRCh38.ensGene.cdsStart\tGRCh38.ensGene.cdsEnd\tGRCh38.ensGene.exonCount\tGRCh38.ensGene.exonStarts\tGRCh38.ensGene.exonEnds\tGRCh38.ensemblToGeneName.value" > Homo_sapiens.GRCh38.84.tsv
+cat tmp.txt >> Homo_sapiens.GRCh38.84.tsv 
+
 
 git clone https://github.com/ChrisMaherLab/INTEGRATE-Vis.git
 cd ~/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/
@@ -12,6 +16,8 @@ wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GR
 wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.X.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz
+gunzip -c Homo_sapiens.GRCh38.dna.chromosome.* > GRCh38_r84.all.fa
+
 
 cd INTEGRATE-Neo-V-1.2.1
 chmod +x install.sh
