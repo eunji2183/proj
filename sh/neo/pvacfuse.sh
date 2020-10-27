eb1cfa9830098396b468c560fea5817fc26a4822
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-27 14:48:26 +0900

    Update pvacfuse.sh

--- sh/neo/pvacfuse.sh
+++ sh/neo/pvacfuse.sh
@@ -2,3 +2,9 @@
 conda create -n integrate-neo
 conda activate integrate-neo
 conda install bwa python gcc ucsc-gtftogenepred
+
+
+cd INTEGRATE-Neo-V-1.2.1
+chmod +x install.sh
+./install.sh -o /home/eunji/proj/0_sh/biosoft/
+gtfToGenePred -genePredExt -geneNameAsName2 
