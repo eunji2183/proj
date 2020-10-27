e68e33adfd84dc1dd8de1f6fcf6c0474ce518e12
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-27 15:36:41 +0900

    Update pvacfuse.sh

--- sh/neo/pvacfuse.sh
+++ sh/neo/pvacfuse.sh
@@ -1,10 +1,14 @@
 #Integrate-NEO
 conda create -n integrate-neo
 conda activate integrate-neo
-conda install bwa python gcc ucsc-gtftogenepred
-
+conda install bwa python gcc_linux-64 ucsc-gtftogenepred
+wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.gtf.gz
+gtfToGenePred -genePredExt -geneNameAsName2 Homo_sapiens.GRCh38.84.gtf Homo_sapiens.GRCh38.84.genePred
 
 cd INTEGRATE-Neo-V-1.2.1
 chmod +x install.sh
 ./install.sh -o /home/eunji/proj/0_sh/biosoft/
-gtfToGenePred -genePredExt -geneNameAsName2 
+
+
+
