267e6184213bf10c162f52b23b3d2b88955fce85
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-10-29 11:49:33 +0900

    Update pvacfuse.sh

--- sh/neo/pvacfuse.sh
+++ sh/neo/pvacfuse.sh
@@ -1,13 +1,30 @@
-#Integrate-NEO
+##Integrate-NEO (GRCh38.99 version 사용) 
+
+#tophat mapping 
+conda create -n tophat2
+conda install tophat 
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz (GRCh38.99.fa 로 고침)
+bowtie2-build GRCh38.99.fa index
+tophat -G Homo_sapiens.GRCh38.99.gtf --transcriptome-index=GRCh38.99.tr GRCh38.99 (dir GRCh38.99.tr 안에 Homo_sapiens.GRCh38.99 로 생김) 
+#tophat.sh 
+cat config | while read id
+do
+tophat -o /HDD2T/eunji/th/${id} --transcriptome-index=/home/eunji/ref/GRCh38.99.tr/Homo_sapiens.GRCh38.99 
+/home/eunji/ref/GRCh38.99.tr/GRCh38.99 
+/HDD2T/jeeh9/RMLS_RNA/trimmed_fq/Paired/${id}-R_R1_P.fq.gz 
+/HDD2T/jeeh9/RMLS_RNA/trimmed_fq/Paired/${id}-R_R2_P.fq.gz
+done
+
+
 conda create -n integrate-neo
 conda activate integrate-neo
 conda install bwa python gcc_linux-64 ucsc-gtftogenepred samtools bedtools matplotlib cmake 
-wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-wget ftp://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.gtf.gz
-gtfToGenePred -genePredExt -geneNameAsName2 Homo_sapiens.GRCh38.84.gtf Homo_sapiens.GRCh38.84.genePred
-cut -f 1-10,12 Homo_sapiens.GRCh38.84.genePred > tmp.txt
-echo -e "#GRCh38.ensGene.name\tGRCh38.ensGene.chrom\tGRCh38.ensGene.strand\tGRCh38.ensGene.txStart\tGRCh38.ensGene.txEnd\tGRCh38.ensGene.cdsStart\tGRCh38.ensGene.cdsEnd\tGRCh38.ensGene.exonCount\tGRCh38.ensGene.exonStarts\tGRCh38.ensGene.exonEnds\tGRCh38.ensemblToGeneName.value" > Homo_sapiens.GRCh38.84.tsv
-cat tmp.txt >> Homo_sapiens.GRCh38.84.tsv 
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-99/gtf/homo_sapiens/Homo_sapiens.GRCh38.99.gtf.gz
+gtfToGenePred -genePredExt -geneNameAsName2 Homo_sapiens.GRCh38.99.gtf Homo_sapiens.GRCh38.99.genePred
+cut -f 1-10,12 Homo_sapiens.GRCh38.99.genePred > tmp.txt
+echo -e "#GRCh38.ensGene.name\tGRCh38.ensGene.chrom\tGRCh38.ensGene.strand\tGRCh38.ensGene.txStart\tGRCh38.ensGene.txEnd\tGRCh38.ensGene.cdsStart\tGRCh38.ensGene.cdsEnd\tGRCh38.ensGene.exonCount\tGRCh38.ensGene.exonStarts\tGRCh38.ensGene.exonEnds\tGRCh38.ensemblToGeneName.value" > Homo_sapiens.GRCh38.99.tsv
+cat tmp.txt >> Homo_sapiens.GRCh38.99.tsv 
 
 
 git clone https://github.com/ChrisMaherLab/INTEGRATE-Vis.git
@@ -15,11 +32,11 @@ cd INTEGRATE-Vis.1.0.0
 chmod +x install.sh
 ./install.sh -o /home/eunji/proj/0_sh/biosoft/integrate-vis
 cd ~/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/
-wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.{1..22}.fa.gz 
-wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.X.fa.gz
-wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
-wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz
-gunzip -c Homo_sapiens.GRCh38.dna.chromosome.* > GRCh38_r84.all.fa
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.{1..22}.fa.gz 
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.X.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz
+gunzip -c Homo_sapiens.GRCh38.dna.chromosome.* > GRCh38_r99.all.fa
 
 
 cd INTEGRATE-Neo-V-1.2.1
@@ -34,32 +51,19 @@ cmake ../Integrate/ -DCMAKE_BUILD_TYPE=release
 make
 cd ~/proj/0_sh/biosoft/INTEGRATE_0_2_6/INTERGRATE-build/bin
 mkdir ./bwts 
-./Integrate mkbwt /home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/GRCh38_r84.all.fa 
+./Integrate mkbwt /home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/GRCh38_r99.all.fa 
 
 #integrate fusion 
-./Integrate fusion /home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/GRCh38_r84.all.fa \
-/home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/gene_model/Homo_sapiens.GRCh38.84.tsv \
+./Integrate fusion /home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/reference_genome/GRCh38_r99.all.fa \
+/home/eunji/proj/0_sh/biosoft/INTEGRATE-Vis/INTEGRATE-Vis.1.0.0/data/gene_model/Homo_sapiens.GRCh38.99.tsv \
 ./bwts/ \
 /home/eunji/proj/RMLS/rna/4.align/hisat2_tran/${id}.sort.bam 
 
-#tophat mapping 
-wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz (GRCh38.99.fa 로 고침)
-bowtie2-build GRCh38.99.fa index
-tophat -G Homo_sapiens.GRCh38.99.gtf --transcriptome-index=GRCh38.99.tr GRCh38.99 (dir GRCh38.99.tr 안에 Homo_sapiens.GRCh38.99 로 생김) 
-#tophat.sh 
-cat config | while read id
-do
-tophat -o /HDD2T/eunji/th/${id} --transcriptome-index=/home/eunji/ref/GRCh38.99.tr/Homo_sapiens.GRCh38.99 
-/home/eunji/ref/GRCh38.99.tr/GRCh38.99 
-/HDD2T/jeeh9/RMLS_RNA/trimmed_fq/Paired/${id}-R_R1_P.fq.gz 
-/HDD2T/jeeh9/RMLS_RNA/trimmed_fq/Paired/${id}-R_R2_P.fq.gz
-done
 
-#Integrate fusion 
 
 
 ------------------------------------------------------------------------------------------------------------------------------------
-#agfusion 
+#agfusion (GRCh38.84 version 사용) 
 conda create -n agfusion 
 conda activate agfusion 
 conda install pyensembl 
