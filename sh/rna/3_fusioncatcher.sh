1a098af64e8e7a32104480d682f2b9d88b4c0ab3
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-11-04 20:09:51 +0900

    Update 3_fusioncatcher.sh

--- sh/rna/3_fusioncatcher.sh
+++ sh/rna/3_fusioncatcher.sh
@@ -5,3 +5,6 @@ echo "start fusioncatcher for ${id}" `date`
 fusioncatcher.py -d /home/eunji/miniconda3/envs/python2/share/fusioncatcher-1.20/db/human_v98/ -i ./1.raw_fq/${id}/ -o ./7.fusion/${id}/ >> ./7.fusion/${id}_fusioncatcher.log 2>&1
 echo "end fusioncatcher for ${id}" `date`
 done 
+
+#fusion만 뽑아내기 
+grep "\*" summary_candidate_fusions.txt | awk '{printf ("%s\n",$2)}' > 15190-MLS.fusion.txt  
