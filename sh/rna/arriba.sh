871557a14dba14c9f2849b7356e2f243c5660d5d
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-11-18 15:13:01 +0900

    Create arriba.sh

---
+++ sh/rna/arriba.sh
@@ -0,0 +1,8 @@
+./run_arriba.sh STAR_index_GRCh38_ENSEMBL93/ 
+ENSEMBL93.gtf GRCh38.fa 
+database/blacklist_hg38_GRCh38_v2.0.0.tsv.gz 
+database/known_fusions_hg38_GRCh38_v2.0.0.tsv.gz 
+database/protein_domains_hg38_GRCh38_v2.0.0.gff3 
+8 
+/home/eunji/proj/RMLS/rna/51921-RMLS-R_1_val_1.fq.gz 
+/home/eunji/proj/RMLS/rna/51921-RMLS-R_2_val_2.fq.gz 
