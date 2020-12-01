331153385bc3e0a301c9720b266f61b20c583053
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-12-01 12:17:09 +0900

    Update count.sh

--- sh/scRNA/count.sh
+++ sh/scRNA/count.sh
@@ -1,2 +1,11 @@
-#trim_galore 
-trim_galore --nextera -o
+
+## trim_galore.sh
+cat config | while read id
+do
+        fq1=./raw_fq/${id}_1.fastq.gz
+        fq2=./raw_fq/${id}_2.fastq.gz
+        echo "start trim_galore for ${id}" `date`
+        trim_galore --nextera --gzip --cores 8 -o ./trim_fq  $fq1  $fq2 >> ./trim_fq/${id}_trim.log 2>&1
+        echo "end trim_galore for ${id}" `date`
+done
+    
