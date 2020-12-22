075151a9303320504667b8bb8ff90e2d09897983
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2020-12-22 19:04:01 +0900

    Update and rename sort_pos.sh to VariantCalling

--- sh/rna/sort_pos.sh
+++
@@ -1,7 +0,0 @@
-#sort_pos.sh
-cat config | while read id 
-do
-echo "start samtools sort pos for ${id}" `date`
-samtools sort -@ 10 ./4.align/hisat2_tran/${id}.bam -o ./4.align/hisat2_tran/${id}_sort.bam 
-echo "end samtools sort pos for ${id}" `date`
-done 
