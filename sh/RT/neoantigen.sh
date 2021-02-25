41e654017e63ef65354f97d416630091e7eb1b33
eunji2183 <68684174+eunji2183@users.noreply.github.com>
2021-02-25 21:29:26 +0900

    Update neoantigen.sh

--- sh/RT/neoantigen.sh
+++ sh/RT/neoantigen.sh
@@ -25,3 +25,16 @@ do
         fastp -i ./raw_fq/${id}_1.fastq.gz -o ./trim_fq/${id}_1.fastq.gz -I ./raw_fq/${id}_2.fastq.gz -O ./trim_fq/${id}_2.fastq.gz --thread=16
 done
 
+##  trim_galore
+cat config | while read id
+do
+        trim_galore --illumina -o ./trim_fq2 ./raw_fq/${id}_1.fastq.gz ./raw_fq/${id}_2.fastq.gz >> ./trim_fq2/${id}_trim.log 2>&1
+done
+
+#trimmomatic
+cat config | while read id 
+do
+trimmomatic PE -phred33 ./raw_fq/${id}_1.fastq.gz ./raw_fq/${id}_2.fastq.gz \
+./trimmomatic/${id}_R1_P.fq.gz ./trimmomatic/${id}_R1_U.fq.gz ./trimmomatic/${id}_R2_P.fq.gz ./trimmomatic/${id}_R2_U.fq.gz \
+ILLUMINACLIP:/home/eunji/miniconda3/envs/IR/share/trimmomatic-0.39-1/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+done
